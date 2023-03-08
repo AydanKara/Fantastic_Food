15f51f96ec11a63be9f44a45b2dcc0a45ba227d4
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-03-08 20:13:40 +0100

    update

--- script.js
+++ script.js
@@ -4,16 +4,48 @@ function mobileMenuFunc() {
     const mobileMenu = document.querySelector("#navbar");
     const closeMobileMenu = document.querySelector("#close-btn");
     const menuListX = document.querySelector(".bi-x-lg");
-    btnOpenMobileMenu.addEventListener("click", function () {
+    const btnHome = document.querySelector("#btn-home");
+    const btnAbout = document.querySelector("#btn-about");
+    const btnRecipes = document.querySelector("#btn-recipes");
+    const btnNews = document.querySelector("#btn-news");
+    const btnBlog = document.querySelector("#btn-blog");
+    const btnContact = document.querySelector("#btn-contact");
 
+    btnOpenMobileMenu.addEventListener("click", function () {
         mobileMenu.style.display = "block";
         btnOpenMobileMenu.style.display = "none";
         closeMobileMenu.style.display = "block";
         menuListX.style.animation = "animate 0.5s alternate-reverse";
     });
-
+    btnHome.addEventListener("click", function () {
+        mobileMenu.style.display = "none";
+    });
+    btnAbout.addEventListener("click", function () {
+        mobileMenu.style.display = "none";
+        btnOpenMobileMenu.style.display = "block";
+        closeMobileMenu.style.display = "none";
+    });
+    btnRecipes.addEventListener("click", function () {
+        mobileMenu.style.display = "none";
+        btnOpenMobileMenu.style.display = "block";
+        closeMobileMenu.style.display = "none";
+    });
+    btnNews.addEventListener("click", function () {
+        mobileMenu.style.display = "none";
+        btnOpenMobileMenu.style.display = "block";
+        closeMobileMenu.style.display = "none";
+    });
+    btnBlog.addEventListener("click", function () {
+        mobileMenu.style.display = "none";
+        btnOpenMobileMenu.style.display = "block";
+        closeMobileMenu.style.display = "none";
+    });
+    btnContact.addEventListener("click", function () {
+        mobileMenu.style.display = "none";
+        btnOpenMobileMenu.style.display = "block";
+        closeMobileMenu.style.display = "none";
+    });
     closeMobileMenu.addEventListener("click", function () {
-
         mobileMenu.style.display = "none";
         btnOpenMobileMenu.style.display = "block";
         closeMobileMenu.style.display = "none";
